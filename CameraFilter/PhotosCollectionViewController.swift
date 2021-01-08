065910e54f1e84756611221dc36f164bc2e91651
ShimuraKeita <Keitagsk8001@gmail.com>
2021-01-09 07:37:07 +0900

    選択した写真の登録と表示を設定。

--- CameraFilter/PhotosCollectionViewController.swift
+++ CameraFilter/PhotosCollectionViewController.swift
@@ -8,10 +8,16 @@
 import Foundation
 import UIKit
 import Photos
+import RxSwift
 
 private let identifier = "PhotosCollectionViewCell"
 
 class PhotosCollectionViewController: UICollectionViewController {
+    
+    private let selectedPhotoSubject = PublishSubject<UIImage>()
+    var selectedPhoto: Observable<UIImage> {
+        return selectedPhotoSubject.asObservable()
+    }
     
     private var images = [PHAsset]()
     
@@ -27,6 +33,24 @@ class PhotosCollectionViewController: UICollectionViewController {
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let selectedAsset = self.images[indexPath.row]
+        PHImageManager.default().requestImage(for: selectedAsset, targetSize: CGSize(width: 300,height: 300), contentMode: .aspectFit, options: nil) { [weak self] image, info in
+            
+            guard let info = info else { return }
+            
+            let isDegradedImage = info["PHImageResultIsDegradedKey"] as! Bool
+            
+            if !isDegradedImage {
+                if let image = image {
+                    self?.selectedPhotoSubject.onNext(image)
+                    self?.dismiss(animated: true, completion: nil)
+                }
+            }
+        }
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
--- CameraFilter/ViewController.swift
+++ CameraFilter/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 import RxSwift
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        guard let navC = segue.destination as? UINavigationController,
+              let photoCVC = navC.viewControllers.first as? PhotosCollectionViewController else {
+            fatalError("Segue destination is not found")
+        }
+        
+        photoCVC.selectedPhoto.subscribe(onNext: { [weak self] photo in
+            
+            self?.photoImageView.image = photo
+        }).disposed(by: disposeBag)
     }
 }
 
