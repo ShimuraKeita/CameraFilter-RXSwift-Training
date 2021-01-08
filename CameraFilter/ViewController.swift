19a7b038c6edccbd1eb7ba69cfe5dc467f930fae
ShimuraKeita <Keitagsk8001@gmail.com>
2021-01-09 08:06:33 +0900

    フィルター設定、Observableへ変更。

--- CameraFilter/ViewController.swift
+++ CameraFilter/ViewController.swift
@@ -10,6 +10,7 @@ import RxSwift
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var applyFilterButton: UIButton!
     @IBOutlet weak var photoImageView: UIImageView!
     
     let disposeBag = DisposeBag()
@@ -29,8 +30,29 @@ class ViewController: UIViewController {
         
         photoCVC.selectedPhoto.subscribe(onNext: { [weak self] photo in
             
-            self?.photoImageView.image = photo
+            DispatchQueue.main.async {
+                self?.updateUI(with: photo)
+            }
         }).disposed(by: disposeBag)
+    }
+    
+    @IBAction func applyFilterButtonPressed() {
+        
+        guard let sourceImage = self.photoImageView.image else {
+            return
+        }
+        
+        filterService().applyFilter(to: sourceImage).subscribe(onNext: { filteredImage in
+            
+            DispatchQueue.main.async {
+                self.photoImageView.image = filteredImage
+            }
+        }).disposed(by: disposeBag)
+    }
+    
+    private func updateUI(with image: UIImage) {
+        self.photoImageView.image = image
+        self.applyFilterButton.isHidden = false
     }
 }
 
